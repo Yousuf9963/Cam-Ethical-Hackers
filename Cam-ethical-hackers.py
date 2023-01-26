f0ff190ad164c2c1397de4428b079d7d3cd5973f
Yousuf9963 <113084773+Yousuf9963@users.noreply.github.com>
2023-01-26 12:19:19 +0300

    Update Cam-ethical-hackers.py

--- Cam-ethical-hackers.py
+++ Cam-ethical-hackers.py
@@ -20,7 +20,7 @@ print("[-] Website: muhammadabdirahman.wixsite.com/yousuf9963blog.")
 
 print("[!] legal disclaimer: Usage of this Program for attacking targets without prior mutual consent is illegal. It is the end user's responsibility to obey all applicable local, state and federal laws. Developers assume no liability and are not responsible for any misuse or damage caused by this program.")
 
-print("Motto: [+] I hope for you good future and i am willing that you will come high effort.")
+print("[+] Motto: I hope for you good future and i am willing that you will come high effort.")
 
 print("")
 
